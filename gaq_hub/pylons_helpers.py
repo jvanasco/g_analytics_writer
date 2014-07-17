93cf0709930cb5a25ad6ac7f0f2db5417d158bc2
jonathan vanasco <jonathan@2xlp.com>
2014-07-17 15:21:53 -0400

    v 0.0.13

--- gaq_hub/pylons_helpers.py
+++ gaq_hub/pylons_helpers.py
@@ -1,36 +1,47 @@
 from . import GaqHub
-from pylons import c 
+from pylons import c
 
-def gaq_setup( account_id , single_push=False ):
-    c._gaq= GaqHub( account_id , single_push=single_push )
+
+def gaq_setup(account_id, single_push=False):
+    c._gaq = GaqHub(account_id, single_push=single_push)
     return c.gaq
 
+
 def gaq_setAccount(account_id):
-    c._gaq.setAccount( account_id )
+    c._gaq.setAccount(account_id)
+
 
 def gaq_setSinglePush(bool_value):
-    c._gaq.setSinglePush( bool_value )
+    c._gaq.setSinglePush(bool_value)
+
 
 def gaq_trackEvent(track_dict):
     c._gaq.trackEvent(track_dict)
-    
-def gaq_setCustomVar(index,name,value,opt_scope=None):
-    c._gaq.setCustomVar(index,name,value,opt_scope=opt_scope)
+
+
+def gaq_setCustomVar(index, name, value, opt_scope=None):
+    c._gaq.setCustomVar(index, name, value, opt_scope=opt_scope)
+
 
 def gaq_setDomainName(domain_name):
     c._gaq.setDomainName(domain_name)
 
+
 def gaq_setAllowLinker(bool_allow):
     c._gaq.setAllowLinker(bool_allow)
 
+
 def gaq_addTrans(track_dict):
     c._gaq.addTrans(track_dict)
-    
+
+
 def gaq_addItem(track_dict):
     c._gaq.addItem(track_dict)
 
+
 def gaq_trackTrans():
     c._gaq.trackTrans()
-    
+
+
 def gaq_as_html():
     c._gaq.as_html()
--- setup.py
+++ setup.py
@@ -7,36 +7,38 @@ from setuptools import find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = ''
-try: 
+try:
     README = open(os.path.join(here, "README.md")).read()
     README = README.split("\n\n", 1)[0] + "\n"
 except:
     pass
 
-requires = []
+requires = [
+    "metadata_utils >=0.0.1",
+]
 
-setup(name="gaq_hub",
-      version="0.0.12",
-      description="Lightweight Google Analytics support",
-      long_description=README,
-      classifiers=[
+setup(
+    name="gaq_hub",
+    version="0.0.13",
+    description="Lightweight Google Analytics support",
+    long_description=README,
+    classifiers=[
         "Intended Audience :: Developers",
         "Framework :: Pylons",
         "Framework :: Pyramid",
         "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
-        ],
-      keywords="web pylons",
-      author="Jonathan Vanasco",
-      author_email="jonathan@findmeon.com",
-      url="https://github.com/jvanasco/gaq_hub",
-      license="MIT",
-      py_modules=['gaq_hub'],
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      tests_require = requires,
-      install_requires = requires,
-      test_suite="tests",
-      )
-
+    ],
+    keywords="web pylons",
+    author="Jonathan Vanasco",
+    author_email="jonathan@findmeon.com",
+    url="https://github.com/jvanasco/gaq_hub",
+    license="MIT",
+    py_modules=['gaq_hub'],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    tests_require = requires,
+    install_requires = requires,
+    test_suite="tests",
+)
